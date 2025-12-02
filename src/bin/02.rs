12a44f505bc07f0dff39d7dc954657ac1fc2e242
Thibault Jochem <thibault@jochem.fr>
2025-12-02 08:56:07 +0100

    Passing day02p2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,17 +3,14 @@ advent_of_code::solution!(2);
 pub fn is_valid_id(id: &u64) -> bool {
     let id_str = id.to_string();
 
-    if id_str.len() %2 == 1 {
-        return true;
-    }
-
     for i in 0..(id_str.len()/2) {
-        if id_str.chars().nth(i).unwrap() != id_str.chars().nth(i + id_str.len()/2).unwrap() {
-            return true;
+        let leftover = id_str.trim_start_matches(&id_str[0..=i]);
+        if leftover.len() == 0 {
+            return false;
         }
     }
 
-    false
+    true
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -40,7 +37,26 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+         let ranges : Vec<&str> = input.trim_ascii().split(",").collect();
+
+     let mut result = 0;
+
+     for r in ranges {
+        dbg!(&r);
+        let mut it = r.split("-").map(|x| x.parse::<u64>().unwrap());
+        let min = it.next().unwrap();
+        let max = it.next().unwrap();
+        for id in min..=max {
+            
+            if !is_valid_id(&id) {
+                println!("{}", id);    
+                result += id;
+            }
+        }
+
+     }
+
+     Some(result)
 }
 
 #[cfg(test)]
@@ -56,6 +72,16 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
+    }
+
+    #[test]
+    fn test_is_valid_id() {
+        assert_eq!(is_valid_id(&1234), true);
+        assert_eq!(is_valid_id(&12341234), false);
+        assert_eq!(is_valid_id(&123123123), false);
+        assert_eq!(is_valid_id(&1212121212), false);
+        assert_eq!(is_valid_id(&1111111), false);
+        assert_eq!(is_valid_id(&38593862), true);
     }
 }
