46af2f9877c54f5407909458edf47f205e127b7a
Thibault Jochem <thibault@jochem.fr>
2025-12-05 16:50:02 +0100

    formatting.

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(3);
 
-pub fn largest_joltage_of_bank(input: &str, digit_count:  u64) -> Option<u64> {
+pub fn largest_joltage_of_bank(input: &str, digit_count: u64) -> Option<u64> {
     let mut result = 0;
     let mut last_index = 0;
 
@@ -8,7 +8,7 @@ pub fn largest_joltage_of_bank(input: &str, digit_count:  u64) -> Option<u64> {
         let mut current_digit = 0;
         let remaining_digits = digit_count - i;
         let mut current_index = last_index;
-        for j in last_index..input.len()-(remaining_digits as usize)+1 {
+        for j in last_index..input.len() - (remaining_digits as usize) + 1 {
             let c = input.chars().nth(j)?;
             let digit = c.to_digit(10)? as u64;
             if digit > current_digit {
@@ -74,9 +74,21 @@ mod tests {
         assert_eq!(largest_joltage_of_bank("234234234234278", 2), Some(78));
         assert_eq!(largest_joltage_of_bank("818181911112111", 2), Some(92));
 
-        assert_eq!(largest_joltage_of_bank("987654321111111", 12), Some(987654321111));
-        assert_eq!(largest_joltage_of_bank("811111111111119", 12), Some(811111111119));
-        assert_eq!(largest_joltage_of_bank("234234234234278", 12), Some(434234234278));
-        assert_eq!(largest_joltage_of_bank("818181911112111", 12), Some(888911112111));
+        assert_eq!(
+            largest_joltage_of_bank("987654321111111", 12),
+            Some(987654321111)
+        );
+        assert_eq!(
+            largest_joltage_of_bank("811111111111119", 12),
+            Some(811111111119)
+        );
+        assert_eq!(
+            largest_joltage_of_bank("234234234234278", 12),
+            Some(434234234278)
+        );
+        assert_eq!(
+            largest_joltage_of_bank("818181911112111", 12),
+            Some(888911112111)
+        );
     }
 }
