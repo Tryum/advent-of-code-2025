f176c0ecdc7d31019f9810bc63d5a7c903a1080e
Thibault Jochem <thibault@jochem.fr>
2025-12-05 16:39:55 +0100

    Passing day03p2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,27 +1,27 @@
 advent_of_code::solution!(3);
 
-pub fn largest_joltage_of_bank(input: &str) -> Option<u64> {
-    let mut first_digit = 0;
-    let mut first_digit_index = 0;
-    let mut second_digit = 0;
-    for i in 0..input.len() - 1 {
-        let c = input.chars().nth(i)?;
-        let digit = c.to_digit(10)? as u64;
-        if digit > first_digit {
-            first_digit = digit;
-            first_digit_index = i;
-        }
-    }
+pub fn largest_joltage_of_bank(input: &str, digit_count:  u64) -> Option<u64> {
+    let mut result = 0;
+    let mut last_index = 0;
 
-    for i in first_digit_index + 1..input.len() {
-        let c = input.chars().nth(i)?;
-        let digit = c.to_digit(10)? as u64;
-        if  digit > second_digit {
-            second_digit = digit;
+    for i in 0..digit_count {
+        let mut current_digit = 0;
+        let remaining_digits = digit_count - i;
+        for j in last_index..input.len()-(remaining_digits as usize)+1 {
+            let c = input.chars().nth(j)?;
+            let digit = c.to_digit(10)? as u64;
+            if digit > current_digit {
+                current_digit = digit;
+                last_index = j;
+                if current_digit == 9 {
+                    break;
+                }
+            }
         }
+        result = result * 10 + current_digit;
+        last_index = last_index + 1;
     }
-
-    Some(first_digit * 10 + second_digit)
+    Some(result)
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -30,7 +30,7 @@ pub fn part_one(input: &str) -> Option<u64> {
         if line.is_empty() {
             continue;
         }
-        if let Some(result) = largest_joltage_of_bank(line) {
+        if let Some(result) = largest_joltage_of_bank(line, 2) {
             total += result;
         }
     }
@@ -38,7 +38,16 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut total = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        if let Some(result) = largest_joltage_of_bank(line, 12) {
+            total += result;
+        }
+    }
+    Some(total)
 }
 
 #[cfg(test)]
@@ -54,14 +63,19 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 
     #[test]
     fn test_largest_joltage_of_bank() {
-        assert_eq!(largest_joltage_of_bank("987654321111111"), Some(98));
-        assert_eq!(largest_joltage_of_bank("811111111111119"), Some(89));
-        assert_eq!(largest_joltage_of_bank("234234234234278"), Some(78));
-        assert_eq!(largest_joltage_of_bank("818181911112111"), Some(92));
+        assert_eq!(largest_joltage_of_bank("987654321111111", 2), Some(98));
+        assert_eq!(largest_joltage_of_bank("811111111111119", 2), Some(89));
+        assert_eq!(largest_joltage_of_bank("234234234234278", 2), Some(78));
+        assert_eq!(largest_joltage_of_bank("818181911112111", 2), Some(92));
+
+        assert_eq!(largest_joltage_of_bank("987654321111111", 12), Some(987654321111));
+        assert_eq!(largest_joltage_of_bank("811111111111119", 12), Some(811111111119));
+        assert_eq!(largest_joltage_of_bank("234234234234278", 12), Some(434234234278));
+        assert_eq!(largest_joltage_of_bank("818181911112111", 12), Some(888911112111));
     }
 }
