f606ca3739d19f196cabc42f0dc989a4a7414be3
Thibault Jochem <thibault@jochem.fr>
2025-12-11 12:14:50 +0100

    Ugly solution for day08 need to optimize at some point !

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -43,8 +43,10 @@ pub fn part_one(input: &str) -> Option<u64> {
     }
     min_distance_pairs.sort_by_key(|k| k.0);
 
-    let mut circuits: Vec<HashSet<usize>> = Vec::new();
-    let mut junction_box_circuits_map: HashMap<usize, usize> = HashMap::new();
+    let mut circuits = HashMap::new();
+    for i in 0..junction_boxes.len() {
+        circuits.insert(i, vec![i]);
+    }
 
     let mut connections = 0;
     for (_, (jb_a_index, jb_b_index)) in min_distance_pairs {
@@ -52,65 +54,97 @@ pub fn part_one(input: &str) -> Option<u64> {
             break;
         }
 
-        if junction_box_circuits_map.contains_key(&jb_a_index)
-            && junction_box_circuits_map.contains_key(&jb_b_index)
-        {
-            if junction_box_circuits_map[&jb_a_index] != junction_box_circuits_map[&jb_b_index] {
-                let circuit_a_index = junction_box_circuits_map[&jb_a_index];
-                let circuit_b_index = junction_box_circuits_map[&jb_b_index];
+        if circuits[&jb_a_index] != circuits[&jb_b_index] {
+            let mut circuit_a = circuits.remove(&jb_a_index).unwrap();
+            let mut circuit_b = circuits.remove(&jb_b_index).unwrap();
 
-                let boxes_to_move: Vec<usize> = circuits[circuit_b_index].iter().cloned().collect();
-                for jb_index in boxes_to_move {
-                    junction_box_circuits_map.insert(jb_index, circuit_a_index);
-                    circuits[circuit_a_index].insert(jb_index);
-                }
-                circuits[circuit_b_index].clear();
-                println!(
-                    "Merged circuits {} and {}",
-                    circuit_a_index, circuit_b_index
-                );
-            } else {
-                println!("Both junction boxes already assigned to circuits, skipping");
-                continue;
+            circuit_a.append(&mut circuit_b);
+
+            for &jb_index in &circuit_a {
+                circuits.insert(jb_index, circuit_a.clone());
             }
-        } else if junction_box_circuits_map.contains_key(&jb_a_index) {
-            let circuit_index = junction_box_circuits_map[&jb_a_index];
-            junction_box_circuits_map.insert(jb_b_index, circuit_index);
-            circuits[circuit_index].insert(jb_b_index);
-            println!(
-                "Assigned junction box {:?} to existing circuit {}",
-                junction_boxes[jb_b_index], circuit_index
-            );
-        } else if junction_box_circuits_map.contains_key(&jb_b_index) {
-            let circuit_index = junction_box_circuits_map[&jb_b_index];
-            junction_box_circuits_map.insert(jb_a_index, circuit_index);
-            circuits[circuit_index].insert(jb_a_index);
-            println!(
-                "Assigned junction box {:?} to existing circuit {}",
-                junction_boxes[jb_a_index], circuit_index
-            );
-        } else {
-            let circuit_index = circuits.len();
-            junction_box_circuits_map.insert(jb_a_index, circuit_index);
-            junction_box_circuits_map.insert(jb_b_index, circuit_index);
-            circuits.push(HashSet::new());
-            circuits[circuit_index].insert(jb_a_index);
-            circuits[circuit_index].insert(jb_b_index);
-            println!(
-                "Created new circuit {} with junction boxes {:?} and {:?}",
-                circuit_index, junction_boxes[jb_a_index], junction_boxes[jb_b_index]
-            );
-        };
 
+            
+        }   
         connections += 1;
-        println!("Connection {}, circuits : {:?}", connections, circuits);
-    }
 
-    None
+        let unique_circuits:HashSet<Vec<usize>>  = HashSet::from_iter(circuits.values().cloned());
+        //println!("circuits: {:?}", unique_circuits);
+    }
+    let unique_circuits:HashSet<Vec<usize>>  = HashSet::from_iter(circuits.values().cloned());
+    let mut sorted_circuits: Vec<_> = unique_circuits.into_iter().collect();
+    sorted_circuits.sort_by(|a, b| b.len().cmp(&a.len()));
+    let top_three: Vec<_> = sorted_circuits.into_iter().take(3).collect();
+    //println!("Top 3 largest circuits: {:?}", top_three);
+    let result: u64 = top_three.iter().map(|circuit| circuit.len() as u64).product();
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+        let mut junction_boxes = Vec::new();
+    for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        let coords: Vec<u64> = line.split(',').map(|s| s.parse().unwrap()).collect();
+        let junction_box = JunctionBox {
+            x: coords[0],
+            y: coords[1],
+            z: coords[2],
+        };
+        junction_boxes.push(junction_box);
+    }
+
+    let mut min_distance_pairs = Vec::new();
+    for i in 0..junction_boxes.len() {
+        for j in (i + 1)..junction_boxes.len() {
+            let box_a = &junction_boxes[i];
+            let box_b = &junction_boxes[j];
+
+            let dx = box_a.x.abs_diff(box_b.x);
+            let dy = box_a.y.abs_diff(box_b.y);
+            let dz = box_a.z.abs_diff(box_b.z);
+
+            let sqare_distance = dx * dx + dy * dy + dz * dz;
+
+            min_distance_pairs.push((sqare_distance, (i, j)));
+        }
+    }
+    min_distance_pairs.sort_by_key(|k| k.0);
+
+    let mut circuits = HashMap::new();
+    for i in 0..junction_boxes.len() {
+        circuits.insert(i, vec![i]);
+    }
+
+    for (_, (jb_a_index, jb_b_index)) in min_distance_pairs {
+
+        if circuits[&jb_a_index] != circuits[&jb_b_index] {
+            let mut circuit_a = circuits.remove(&jb_a_index).unwrap();
+            let mut circuit_b = circuits.remove(&jb_b_index).unwrap();
+
+            circuit_a.append(&mut circuit_b);
+
+            for &jb_index in &circuit_a {
+                circuits.insert(jb_index, circuit_a.clone());
+            }
+
+            
+        }
+
+        let unique_circuits:HashSet<Vec<usize>>  = HashSet::from_iter(circuits.values().cloned());
+        if unique_circuits.len() == 1 {
+            return Some(junction_boxes[jb_a_index].x * junction_boxes[jb_b_index].x);
+        }
+        //println!("circuits: {:?}", unique_circuits);
+    }
+    let unique_circuits:HashSet<Vec<usize>>  = HashSet::from_iter(circuits.values().cloned());
+    let mut sorted_circuits: Vec<_> = unique_circuits.into_iter().collect();
+    sorted_circuits.sort_by(|a, b| b.len().cmp(&a.len()));
+    let top_three: Vec<_> = sorted_circuits.into_iter().take(3).collect();
+    //println!("Top 3 largest circuits: {:?}", top_three);
+    let result: u64 = top_three.iter().map(|circuit| circuit.len() as u64).product();
+    Some(result)
 }
 
 #[cfg(test)]
@@ -126,6 +160,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(25272));
     }
 }
